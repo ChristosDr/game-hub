1b8afba45f21ec87f96d985ec8ebfad428b7ef45
ChrisD323 <drikosx323@gmail.com>
2025-11-26 20:27:53 +0200

    Create a generic data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,13 @@
-import useGenres from "@/hooks/useGenres";
+import useData from "@/hooks/useData";
+import useGenres, { Genre } from "@/hooks/useGenres";
 import { ul } from "framer-motion/client";
 import React from "react";
 
 const GenreList = () => {
-  const { genres, error, isLoading } = useGenres();
+  const { data, error, isLoading } = useGenres();
   return (
     <ul>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <li key={genre.id}>{genre.name}</li>
       ))}
     </ul>
