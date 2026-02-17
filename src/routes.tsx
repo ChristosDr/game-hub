4635f6dc75ccf2688c366122a4f8afefb0879f38
ChrisD323 <drikosx323@gmail.com>
2026-02-17 10:30:23 +0200

    Hnadle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import GameDetailePage from "./pages/GameDetailePage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       { index: true, element: <HomePage></HomePage> },
       { path: "games/:id", element: <GameDetailePage></GameDetailePage> },
